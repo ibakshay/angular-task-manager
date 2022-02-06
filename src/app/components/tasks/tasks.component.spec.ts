5e8a544f307e7e9d374ff73a07f2faa95647067d
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2022-02-06 17:09:47 +0530

    added test for task component

--- src/app/components/tasks/tasks.component.spec.ts
+++ src/app/components/tasks/tasks.component.spec.ts
@@ -1,26 +1,75 @@
-/*
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { TasksComponent } from './tasks.component';
+import { TaskService } from '../../services/task.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import * as Rx from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 describe('TasksComponent', () => {
   let component: TasksComponent;
   let fixture: ComponentFixture<TasksComponent>;
+  let taskService: TaskService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TasksComponent ]
+      imports: [
+        HttpClientTestingModule
+      ],
+      declarations: [TasksComponent],
+      providers: [
+        TaskService
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
+
   beforeEach(() => {
     fixture = TestBed.createComponent(TasksComponent);
-    component = fixture.componentInstance;
+    component = fixture.debugElement.componentInstance;
+    taskService = fixture.debugElement.injector.get(TaskService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call ngOnit', () => {
+    // @ts-ignore
+    let spyGetTasks = spyOn(component, 'getTasks').and.returnValue([]);
+    component.ngOnInit();
+    expect(component.tasks).toEqual([]);
+  });
+
+  it('Should call getTasks and return an empty array', fakeAsync(() => {
+    let spyGetTasks = spyOn(taskService, 'getTasks').and.callFake(() => {
+      return Rx.of([]).pipe(delay(100));
+    });
+    component.getTasks();
+    tick(100);
+    expect(component.tasks).toEqual([]);
+  }));
+
+  it('Should call getTasks and return an array with one elememt', fakeAsync(() => {
+    let spyGetTasks = spyOn(taskService, 'getTasks').and.callFake(() => {
+      return Rx.of([{
+        id: 2,
+        text: 'Meeting at College',
+        day: 'May 6th at 2:30pm',
+        reminder: true
+      }]).pipe(delay(100));
+    });
+    component.getTasks();
+    tick(100);
+    expect(component.tasks).toEqual([{
+      id: 2,
+      text: 'Meeting at College',
+      day: 'May 6th at 2:30pm',
+      reminder: true
+    }]);
+  }));
+
+  
 });
-*/
+
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -15,6 +15,10 @@ export class TasksComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getTasks();
+  }
+
+  getTasks() {
     this.taskService.getTasks().subscribe(tasks => this.tasks = tasks);
   }
 
